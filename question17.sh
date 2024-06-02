9d30119d2463e6d5816cb6ed300236177acb5e51
a1832586 <a1832586@student.adelaide.edu.au>
2024-06-02 19:06:36 +0930

    modify

--- question17.sh
+++ question17.sh
@@ -11,15 +11,13 @@ git commit -m "cp dir3/bar"
 
 git checkout branch1
 mv dir1/dir2/foo dir1/
-cd dir1
-rmdir dir2
-touch ./newfile1
-git add foo ./newfile1
+rmdir dir1/dir2
+touch newfile1
+git add dir1/foo newfile1
 git rm dir2/foo
 git commit -m"Modify branch1"
 
 git checkout branch2
-cd ..
 mv dir1/dir2/foo dir1/dir2/foo_modified
 rm dir3/bar
 touch dir3/newfile2
